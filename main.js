69aebfc29451445f0c9a259d834f7c07a5a224cb
Ahmedatia99 <atyaa629@gmail.com>
2022-07-30 14:56:37 +0200

    w

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let editor = document.querySelector('.link')
 let editorBtn = document.querySelector('.linkBtn')
 let startNow = document.getElementById('start')
-
+// open another page
 editor.onclick = function () {
   window.open('/login/login.html')
 }
@@ -16,17 +16,22 @@ let switchPrice = document.getElementById('check')
 let discText = document.getElementById('proo')
 let discSpan = document.getElementById('disc')
 let descPra = document.getElementById('disc-pra')
-
+let discountDiv = document.getElementById('discount')
+//price
 switchPrice.addEventListener('change', () => {
   if (switchPrice.checked) {
     discText.innerHTML = '$6 '
     discText.appendChild(discSpan)
     descPra.style.cssText = 'display:block'
     discText.style.cssText = 'margin-bottom:0'
+    discountDiv.style.cssText = 'display:block'
   } else {
     discText.innerHTML = '$9 '
     discText.appendChild(discSpan)
     descPra.style.cssText = 'display:none'
     discText.style.cssText = 'margin-bottom:15px'
+    discountDiv.style.cssText = 'display:none'
   }
 })
+
+//discount
